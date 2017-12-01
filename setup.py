91e02b61ae64990e55dce41a46d4985fcec7406c
jesly <jesly.varghese@gmail.com>
2017-12-01 22:33:17 +0530

    updated packages

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from setuptools import find_packages
 
 def readme():
     with open('README.rst') as f:
@@ -15,5 +16,17 @@ setup(
         license='MIT',
         packages=['migrator', 'migrator.migrations', 'migrator.providers'],
         zip_safe=False,
-        install_requires=['boto3', 'azure-storage', 'azure-nspkg', 'cryptography', 'ipaddress',  'click', 'tqdm', 'python-json-logger'],
+        install_requires=['boto3',
+                          'azure-storage',
+                          'azure-nspkg',
+                          'cryptography',
+                          'azure-common>=1.1.5',
+                          'cryptography',
+                          'python-dateutil',
+                          'requests',
+                          'azure-storage-common>=0.37.1,<0.38.0',
+                          'ipaddress', 
+                          'click',
+                          'tqdm',
+                          'python-json-logger'],
         entry_points={'console_scripts': ['migrate-to-azure=migrator.commandline:run']})
